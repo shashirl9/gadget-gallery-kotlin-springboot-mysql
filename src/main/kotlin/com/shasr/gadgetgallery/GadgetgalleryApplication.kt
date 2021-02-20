b81b4cceaa671d1c4bd12c334d2fd67680b97b1e
shashirl9 <shashidhar.lucky9@gmail.com>
2021-02-20 12:56:57 +0530

    H2 and OpenAPI 3.0 configuration

--- src/main/kotlin/com/shasr/gadgetgallery/GadgetgalleryApplication.kt
+++ src/main/kotlin/com/shasr/gadgetgallery/GadgetgalleryApplication.kt
@@ -1,18 +1,12 @@
 package com.shasr.gadgetgallery
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @OpenAPIDefinition
 @SpringBootApplication
-class GadgetgalleryApplication : CommandLineRunner {
-
-    override fun run(vararg args: String?) {
-        println("SpringBoot - Kotlin application started..!")
-    }
-}
+class GadgetgalleryApplication
 
 fun main(args: Array<String>) {
     runApplication<GadgetgalleryApplication>(*args)
